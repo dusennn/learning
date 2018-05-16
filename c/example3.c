70c0199add7eff397e4ba4b68c48c9d8426634ca
Sdu <sdu@shendu.info>
2018-05-16 16:05:51 +0800

    completed example5

--- c/example3.c
+++ c/example3.c
@@ -1,15 +1,19 @@
 # include <stdio.h>
+# include <limits.h>
 
 /*
 	题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
 */
 
-int main(){
-	long x, n, m;
 
-	268
-	n > m ;n * n + m * m  = 100 + 168;
-	
+int main(){
+	int maxnum = INT_MAX;
+	int minnum = INT_MIN;
+	int num;
+
+	for(int i = minnum; i <= maxnum; i++){
+		//TODO
+	}
 
 	return 0;
 }
--- c/example5.c
+++ c/example5.c
@@ -5,13 +5,47 @@
 */
 
 int main(){
-	int x, y, z;
+	int nums[3], temp;
 
 	printf("请输入三个整数：\n");
-	scanf("%d", &x);
-	scanf("%d", &y);
-	scanf("%d", &z);
+	scanf("%d", &nums[0]);
+	scanf("%d", &nums[1]);
+	scanf("%d", &nums[2]);
 
-	if(x > y)
+	//print origin nums
+	for(int i = 0; i < 3; i++){
+		printf("%d\t", nums[i]);
+	}
+	printf("\n");
+
+	//冒泡排序
+	// for(int i = 0; i < 3; i++){
+	// 	for(int j = 0; j < 3 - i; j++){
+	// 		if(nums[j] > nums[j + 1]){
+	// 			int temp = nums[j];
+	// 			nums[j] = nums[j + 1];
+	// 			nums[j + 1] = temp;
+	// 		}
+	// 	}
+	// }
+
+
+	//直接插入排序
+	for(int i = 0; i < 3; i ++){
+		temp = nums[i];
+		for(int j = 0; j < i; j++){
+			if(temp < nums[j]){
+				nums[i] = nums[j];
+				nums[j] = temp;
+			}
+		}
+	}
+
+	//print sorted nums
+	for(int i = 0; i < 3; i++){
+		printf("%d\t", nums[i]);
+	}
+	printf("\n");
+
 	return 0;
 }
