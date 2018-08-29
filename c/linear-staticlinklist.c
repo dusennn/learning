c75f968fe53bb2e3ef2189c006c3a9d20963d4e7
Sen Du <951545814@qq.com>
2018-08-29 16:47:51 +0800

    add method(sll del)

--- c/linear-staticlinklist.c
+++ c/linear-staticlinklist.c
@@ -56,7 +56,7 @@ int listInsert(SLList s, int i, ElemType e){
     *通过链表最后一个元素的指针得到第一个元素的位置，
     *然后循环i-1次，得到要插入元素的前一个元素的游标。*/
     int pre = s[MAXSIZE-1].cur;
-    for(int j=1; j<i-1; j++){
+    for(int j=0; j<i-1; j++){
         pre = s[pre].cur;
     }
     
@@ -68,16 +68,65 @@ int listInsert(SLList s, int i, ElemType e){
     return OK;
 }
 
+int getElem(SLList s, int i, ElemType *e){
+    int len = MAXSIZE-1;
+    if(i<1||i>len||!s[len].cur) return ERROR;
+
+    int cursor = s[len].cur;
+    for(int j=0; j<i-1; j++){
+        cursor = s[cursor].cur;
+    }
+    if(cursor!=i) return ERROR;
+
+    *e = s[cursor].data;
+    return OK;
+}
+
+int listDel(SLList s, int i, ElemType *e){
+    int len = MAXSIZE-1;
+    if(i<1||i>len||!s[len].cur) return ERROR;
+    
+    //获取待删除元素的前一个元素的游标。
+    int cursor = s[len].cur;
+    for(int j=1; j<i-1; j++){
+        cursor = s[cursor].cur;
+    }
+    if(s[cursor].cur!=i) return ERROR;
+    
+    //首先把待删除元素的游标指向列表第一个空元素，
+    //然后把列表第0个位置的游标指向待删除元素的位置，
+    //最后把该元素删除，再把待删除元素的前一个元素游标指向待删除元素的后一个元素位置。 
+    int ncur = s[cursor].cur;
+    s[cursor].cur = s[s[cursor].cur].cur;
+    *e = s[ncur].data;
+    s[ncur].cur = s[0].cur;
+    s[ncur].data = 0;
+    s[0].cur = i;
+    
+    return OK;
+
+}
+
 int main(){
     SLList s;
     initList(s);
     randomData(s);
-
-    printf("before insert...\n");
+    printf("before del...\n");
     print(s);
     ElemType e;
-    e = 10;
-    listInsert(s, 1, e);
-    printf("after insert...\n");
+    listDel(s, 2, &e);
+    printf("after del...\n");
     print(s);
+    printf("Point %d: %d\n", 2, e);
+   // print(s);
+   // ElemType e;
+   // getElem(s, 2, &e);
+   // printf("Point %d: %d\n", 2, e);
+   // printf("before insert...\n");
+   // print(s);
+   // ElemType e;
+   // e = 10;
+   // listInsert(s, 1, e);
+   // printf("after insert...\n");
+   // print(s);
 }
