90a914c4b902631868b8718b01b45e4684389540
Sen Du <951545814@qq.com>
2018-09-07 17:50:47 +0800

    add new file(tree sibling)

---
+++ c/tree-sibling.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+typedef int ElemType;
+typedef struct CSNode{
+    ElemType data;
+    struct CSNode *firstChild, *nextSibling;
+} CSNode, *CSNodePrt;
+
+int main(){
+    
+    return 0;
+}
