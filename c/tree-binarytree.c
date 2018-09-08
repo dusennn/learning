6deb67fe93a4b9b81643a3d6ffd52075d513b144
Sen Du <951545814@qq.com>
2018-09-08 15:48:20 +0800

    add new file(binary tree)

---
+++ c/tree-binarytree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+#define ERROR -1
+#define OK 1
+
+typedef int ElemType;
+typedef struct BiTNode{
+    ElemType data;
+    struct BiTNode *lchild, *rchild;
+}BiTNode, *BiTree;
+
+int main(){
+    
+    return 0;
+}
