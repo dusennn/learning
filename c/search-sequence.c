3b5ab90abb404a0c8b00bb3193feb7fef08f17c4
Sen Du <951545814@qq.com>
2018-10-03 17:45:46 +0800

    Improve sequence search

--- c/search-sequence.c
+++ c/search-sequence.c
@@ -7,23 +7,38 @@
 
 typedef int Status;
 
+//「数组下标从1开始」
+
 //data: 数据集
 //len: 数据长度
 //key: 关键字
 Status sqSearch(int *data, int len, int key){
-    for(int i=0; i<len; i++){
+    for(int i=1; i<len; i++){
         if(data[i] == key) return OK;
     }
     return ERROR;
+}
+
+Status sqSearch2(int *data, int len, int key){
+    int n = len;
+    data[0] = key;
+    while(data[n] != key){
+        n--;
+    }
+    return n;
 }
 
 int main(){
     int *data;
     data = (int *)malloc(10 * sizeof(int));
     srand(time(0));
-    for(int i=0; i<10; i++){
-        data[i] = rand() % 100; 
+    for(int i=1; i<=9; i++){
+        data[i] = rand() % 20; 
     }
-    Status s = sqSearch(data, 8, 3);
+
+    Status s = sqSearch(data, 9, 3);
+    printf("Status: %d\n", s);
+
+    s = sqSearch2(data, 9, 3);
     printf("Status: %d\n", s);
 }
