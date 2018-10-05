9302052c263916a9aa3900d85d3301289913666e
Sen Du <951545814@qq.com>
2018-10-05 13:20:02 +0800

    插值查找(existing bugs)

--- c/search-static-sequence_3.c
+++ c/search-static-sequence_3.c
@@ -26,14 +26,34 @@ Status binarySearch(int *list, int len, int key){
 
 }
 
+Status interSearch(int *list, int len, int key){
+    int low, mid, high;
+    low = 1;
+    high = len;
+    
+    while(low <= high){
+        mid = ((key - list[low]) / (list[high] - list[low])) * (high - low + 1);
+
+        if(list[mid] == key){
+            return OK;
+        }else if(list[mid] < key){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return ERROR;
+}
+
 int main(){
     //研究算法时，为了便于理解，下标默认1开始。
     int len = 10;
     int list[11] = {0, 1, 3, 4, 8, 9, 12, 15, 19, 23, 29};
-    int key = 4;
+    int key = 2;
 
     Status s;
-    s = binarySearch(list, len, key);
+    //s = binarySearch(list, len, key);
+    s = interSearch(list, len, key);
     printf("Status:%d\n", s);
     return 0;
 }
