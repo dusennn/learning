12c07a3f86e4a6de5f9c7399d59af275e4bd9297
Sdu <sdu@shendu.info>
2019-02-09 11:00:50 +0800

    golang foreach

--- go/4.3.go
+++ go/4.3.go
@@ -6,11 +6,26 @@ import (
 
 func main() {
     fmt.Println("https://zhidao.baidu.com/question/744094337901801772.html?qbl=relate_question_0&word=%C8%E7%BA%CE%B0%BE%B0%D7%B4%D7")
-    
+
     for x:=1; x<=9; x++ {
         for y:=1; y<=x; y++ {
             fmt.Printf("%d*%d=%d ", x, y, x*y)
         }
         fmt.Println()
+    }
+
+    fmt.Println()
+
+    c := make(chan int)
+
+    go func(){
+        c <- 1
+        c <- 2
+        c <- 3
+        close(c)
+    }()
+
+    for v := range c {
+        fmt.Println(v)
     }
 }
