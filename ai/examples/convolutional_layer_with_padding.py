f2c99a287bdaaa235876996345efd8863e091b95
杜森 <dusen@hex-tech.net>
2020-10-11 11:18:47 +0800

    Add examples that convolutional layer with padding.

---
+++ ai/examples/convolutional_layer_with_padding.py
@@ -0,0 +1,22 @@
+import torch
+
+
+def main():
+    in_channels, out_channels = 5, 10
+    width, height = 100, 100
+    kernel_size = 3
+    batch_size = 1
+
+    inputs = torch.randn(batch_size, in_channels, width, height)
+    conv_layer = torch.nn.Conv2d(
+        in_channels, out_channels, kernel_size=kernel_size, padding=2,
+    )
+
+    outputs = conv_layer(inputs)
+
+    print(inputs.shape)
+    print(outputs.shape)
+    print(conv_layer.weight.shape)
+
+if __name__ == "__main__":
+    main()
