d8f80ee9e38fc592f1b8145be0750634f65627a4
杜森 <dusen@hex-tech.net>
2020-10-11 14:40:53 +0800

    a more complex neural network

--- ai/mnist/main.py
+++ ai/mnist/main.py
@@ -47,6 +47,36 @@ class CNNet(torch.nn.Module):
         x = self.fc3(x)
         return x
 
+class CNNet2(torch.nn.Module):
+    '''
+    Try a more complex CNN:
+        - Conv2d Layer * 3
+        - ReLU Layer * 3
+        - MaxPooling Layer * 3
+        - Linear Layer * 3
+    '''
+    def __init__(self):
+        super(CNNet2, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=3)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=3)
+        self.conv3 = torch.nn.Conv2d(20, 40, kernel_size=3)
+        self.pooling1 = torch.nn.MaxPool2d(2)
+        self.pooling2 = torch.nn.MaxPool2d(1)
+        self.fc1 = torch.nn.Linear(360, 180)
+        self.fc2 = torch.nn.Linear(180, 90)
+        self.fc3 = torch.nn.Linear(90, 10)
+        self.relu = torch.nn.ReLU()
+    
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.pooling1(self.relu(self.conv1(x)))
+        x = self.pooling1(self.relu(self.conv2(x)))
+        x = self.pooling2(self.relu(self.conv3(x)))
+        x = x.view(batch_size, -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
 
 class MnistModel(object):
     def __init__(self):
@@ -84,7 +114,8 @@ class MnistModel(object):
     
     def __build_net(self):
         # self.nnet = NNet()
-        self.nnet = CNNet()
+        # self.nnet = CNNet()
+        self.nnet = CNNet2()
         self.nnet.to(self.device)
 
     def __criterion_and_optimizer(self):
