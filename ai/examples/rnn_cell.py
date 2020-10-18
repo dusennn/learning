3e2a7ee15cd5bfc2cbeb29f85f242a4303b84e05
杜森 <dusen@hex-tech.net>
2020-10-18 14:16:39 +0800

    Using torch rnn cell

--- ai/examples/rnn_cell.py
+++ ai/examples/rnn_cell.py
@@ -1,26 +1,60 @@
 import torch
 
 
+class Net(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, batch_size):
+        super(Net, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+
+        self.cell = torch.nn.RNNCell(input_size, hidden_size)
+
+    def forward(self, _input, hidden):
+        return self.cell(_input, hidden)
+
+    def init_hidden(self):
+        return torch.zeros(self.batch_size, self.hidden_size)
+
+
 def main():
-    batch_size = 1
-    seq_len = 3
     input_size = 4
-    hidden_size = 2
+    hidden_size = 4
+    batch_size = 1
 
-    cell = torch.nn.RNNCell(input_size=input_size, hidden_size=hidden_size)
+    idx2char = ['h', 'e', 'l', 'o']
+    x_data = [0, 1, 2, 2, 3] # hello
+    y_data = [3, 0, 2, 3, 2] # ohlol
 
-    #(seq, batch, features)
-    dataset = torch.rand(seq_len, batch_size, input_size)
-    hidden = torch.zeros(batch_size, hidden_size)
+    one_hot_lookup = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ]
+    x_one_hot = [one_hot_lookup[x] for x in x_data]
 
-    for idx, inputs in enumerate(dataset, 0):
-        print("="*20, idx, "="*20)
-        print("input size", inputs.shape)
+    inputs = torch.Tensor(x_one_hot).view(-1, batch_size, input_size)
+    labels = torch.LongTensor(y_data).view(-1, 1)
 
-        hidden = cell(inputs, hidden)
+    net = Net(input_size, hidden_size, batch_size)
 
-        print("output size", hidden.shape)
-        print(hidden)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+
+    for epoch in range(100):
+        loss = 0
+        criterion.zero_grad()
+        hidden = net.init_hidden()
+        print('Predicted string: ', end='')
+        for _input, label in zip(inputs, labels):
+            hidden = net(_input, hidden)
+            loss += criterion(hidden, label)
+            _, idx = hidden.max(dim=1)
+            print(idx2char[idx.item()], end='')
+        loss.backward()
+        optimizer.step()
+        print(', Epoch[%d/15] loss=%.4f' % (epoch + 1, loss.item()))
 
 
 if __name__ == "__main__":
