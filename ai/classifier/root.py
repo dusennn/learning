c3c5b3e44c0db5cd2683fa01f4978412c205a4e8
杜森 <dusen@hex-tech.net>
2020-10-24 15:55:44 +0800

    Classifier questions

--- ai/classifier/root.py
+++ ai/classifier/root.py
@@ -1,4 +1,4 @@
-import time
+import time, torch, math
 import matplotlib.pyplot as plt
 
 
@@ -17,3 +17,35 @@ def evaluation(acc_list):
     plt.ylabel('Accuracy')
     plt.grid()
     plt.show()
+
+
+def name2list(name):
+    arr = [ord(c) for c in name]
+    return arr, len(arr)
+
+
+def create_tensor(tensor, use_gpu=False):
+    if use_gpu:
+        device = torch.device("cude:0")
+        tensor =tensor.to(device)
+    return tensor
+
+
+def make_tensors(names, countries, use_gpu=False):
+    sequences_and_lengths = [name2list(name) for name in names]
+    name_sequences = [sl[0] for sl in sequences_and_lengths]
+    seq_lengths = torch.LongTensor([sl[1] for sl in sequences_and_lengths])
+    countries = countries.long()
+
+    seq_tensor = torch.zeros(len(name_sequences), seq_lengths.max()).long()
+    for idx, (seq, seq_len) in enumerate(zip(name_sequences, seq_lengths), 0):
+        seq_tensor[idx, :seq_len] = torch.LongTensor(seq)
+
+    seq_lengths, perm_idx = seq_lengths.sort(dim=0, descending=True)
+    seq_tensor = seq_tensor[perm_idx]
+    countries = countries[perm_idx]
+    
+    return (
+        create_tensor(seq_tensor, use_gpu), create_tensor(seq_lengths, use_gpu), 
+        create_tensor(countries, use_gpu)
+    )
