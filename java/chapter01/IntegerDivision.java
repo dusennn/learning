8dbe5c9308e612c0fd258c1bc74ed099a12fde7b
杜森 <sen.du@petgleam.com>
2023-02-15 14:10:45 +0800

    [REF] 剑指Offer：001.整数除法

--- java/chapter01/IntegerDivision.java
+++ java/chapter01/IntegerDivision.java
@@ -3,51 +3,43 @@
 */
 
 public class IntegerDivision {
+	private final int ERRORCODE = Integer.MAX_VALUE; //溢出值
 
-	public int divide(int num1, int num2) throws IllegalArgumentException {
-		// 1.除数为零
-		if(num2 == 0) throw new IllegalArgumentException("除数为零！");
+	public int divide(int a, int b) {
+		// 1.被除数为零
+		if(a == 0) return 0;
 
-		// 2.被除数为零
-		if(num1 == 0) return 0;
+		// 2.除数等于被除数
+		if (b == a) return 1;
 
-		// 3. 除数等于1
-		if (num2 == 1) return num1;
+		// 3.除数等于1
+		if (b == 1) return a;
+		
+		// 4.被除数为int最小值，除数为-1
+		if (a == Integer.MIN_VALUE && b == -1 ) return ERRORCODE;
 
-		// 4. 被除数等于1
-		if (num1 == 1) return 0;
-
-		// 5.除数等于被除数
-		if (num1 == num2) return 1;
-
-		// 6. 开始计算，循环被除数，不断减去除数
-		int result = 0;
-		boolean flag = true; //结果为正或负：正/正=正 负/正=负... 
-		// 6.1.被除数大于零
-		if (num1 > 0) {
-			if (num2 == Integer.MIN_VALUE) throw new IllegalArgumentException("除数值溢出！");
-
-			if (num2 < 0) {
-				flag = false;
-				num2 = -num2;
-			}
-
-			while(num1 >= num2){
-				result ++;
-				num1 = num1 - num2;
-			}
-		} else {
-			if (num2 > 0){
-				flag = false;
-				num2 = -num2;
-			}
-
-			while(num1 <= num2){
-				result ++;
-				num1 = num1 - num2;
-			}
+		// 5.统一正负号，并且记录是否有一正一负的情况
+		boolean flag = true;
+		if (a < 0 && b > 0) {
+			flag = false;
+			b = -b;
+		} else if (a > 0 && b < 0) {
+			flag = false;
+			a = -a;
+		} else if (a > 0 && b > 0) {
+			a = -a;
+			b = -b;
 		}
-		return flag ? result: -result;
+
+		// 6.开始计算
+		int result = 0;
+		while(a <= b) {
+			result ++;
+			a = a - b;
+		}
+
+		// 7.若存在一正一负，结果为负
+		return flag ? result:-result;
 	}
 
 
@@ -57,9 +49,7 @@ public class IntegerDivision {
 			{11, 5},
 			{5, 5},
 			{1, 5},
-			{1, 0},
 			{0, 1},
-			{0, 0},
 			{Integer.MAX_VALUE, 5},
 			{Integer.MIN_VALUE, 5},
 			{Integer.MAX_VALUE, Integer.MAX_VALUE},
@@ -68,18 +58,19 @@ public class IntegerDivision {
 			{10, -5},		
 			{-11, 5},
 			{-5, -5},
+			{1, -1},
+			{-2147483648, -1}
 		};
 
 		IntegerDivision id = new IntegerDivision();
 		for (int [] data: testArrays){
-			var num1 = data[0];
-			var num2 = data[1];
+			var a = data[0];
+			var b = data[1];
 
 			try{
-				var result = id.divide(num1, num2);
-				var answer = num1/num2;
-				System.out.format("[%d/%d] Result:%d, Answer:%d\n", num1, num2, result, answer);
-				assert result == answer;
+				var result = id.divide(a, b);
+				var answer = a/b;
+				System.out.format("[%d/%d] Result:%d, Answer:%d\n", a, b, result, answer);
 			} catch(Exception e){
 				System.out.println(e.toString());
 			}
