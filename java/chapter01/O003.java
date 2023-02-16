f6ce2e785881834859226c188115a424966bf4ad
杜森 <sen.du@petgleam.com>
2023-02-16 10:28:27 +0800

    [IMP] Add doc

--- java/chapter01/O003.java
+++ java/chapter01/O003.java
@@ -10,6 +10,17 @@ public class O003{
 		int input = this.systemIn();
 		if (input <= 0) return new int[]{0};
 
+		/**
+		 * 主要利用十进制转二进制的方法：
+		100转换为二进制: 
+		100/2=50....(余数为0); 
+		50/2=25.....(余数为0); 
+		25/2=12.....(余数为1); 
+		12/2=6......(余数为0); 
+		6/2=3.......(余数为0); 
+		3/2=1.......(余数为1); 
+		1/2=0.......(余数为1); 
+		 */
 		int []result = new int[input+1];
 		for(int i = 0; i <= input; i++){
 			for(int num = i; num > 0; num = num / 2) {
