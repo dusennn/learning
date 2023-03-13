99687ed0437872ae85b06229d618a89b943b5834
杜森 <sen.du@petgleam.com>
2023-03-13 17:16:34 +0800

    [ADD] 2043. 简易银行系统

---
+++ leetcode/T2043.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/**
+ * 2043. 简易银行系统
+ */
+class Bank {
+    private long[] balance;
+    private int n;
+
+    public Bank(long[] balance) {
+        this.n = balance.length;
+        this.balance = new long[this.n+1];
+        for (int i = 0; i < balance.length; i++) {
+            this.balance[i+1] = balance[i];
+        }
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        // 账号不存在
+        if (!this.exists(account1) || !this.exists(account2)) return false;
+        
+        // 钱不够
+        if (this.balance[account1] < money) return false;
+
+        // 开始转账
+        this.balance[account1] -= money;
+        this.balance[account2] += money;
+        
+        return true;
+    }
+    
+    public boolean deposit(int account, long money) {
+        // 账号不存在
+        if (!this.exists(account)) return false;
+        
+        // 开始存款
+        this.balance[account] += money;
+
+        return true;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        // 账号不存在
+        if (!this.exists(account)) return false;
+
+        // 钱不够
+        if (this.balance[account] < money) return false;
+
+        // 开始取款
+        this.balance[account] -= money;
+
+        return true;
+    }
+
+    // 账号是否存在
+    private boolean exists(int account) {
+        if (account < 1 || account > this.n) {
+            return false;
+        }
+        return true;
+    }
+}
