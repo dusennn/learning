16c54710a815b03ca083ffbd06f482ff35650bb1
杜森 <sen.du@petgleam.com>
2023-03-13 17:37:52 +0800

    [ADD] 2043. 简易银行系统（线程安全）

--- leetcode/T2043.java
+++ leetcode/T2043.java
@@ -1,60 +1,64 @@
 import java.util.*;
 
 /**
- * 2043. 简易银行系统
+ * 2043. 简易银行系统（线程安全）
  */
-class Bank {
+class T2043 {
     private long[] balance;
-    private int n;
 
     public Bank(long[] balance) {
-        this.n = balance.length;
-        this.balance = new long[this.n+1];
+        this.balance = new long[balance.length+1];
         for (int i = 0; i < balance.length; i++) {
             this.balance[i+1] = balance[i];
         }
     }
     
     public boolean transfer(int account1, int account2, long money) {
-        // 账号不存在
-        if (!this.exists(account1) || !this.exists(account2)) return false;
-        
-        // 钱不够
-        if (this.balance[account1] < money) return false;
+    	synchronized(this.balance) {
+    		// 账号不存在
+	        if (!this.exists(account1) || !this.exists(account2)) return false;
+	        
+	        // 钱不够
+	        if (this.balance[account1] < money) return false;
 
-        // 开始转账
-        this.balance[account1] -= money;
-        this.balance[account2] += money;
-        
-        return true;
+	        // 开始转账
+	        this.balance[account1] -= money;
+	        this.balance[account2] += money;
+	        
+	        return true;	
+    	}
     }
     
     public boolean deposit(int account, long money) {
-        // 账号不存在
-        if (!this.exists(account)) return false;
-        
-        // 开始存款
-        this.balance[account] += money;
+        synchronized(this.balance) {
+        	// 账号不存在
+	        if (!this.exists(account)) return false;
+	        
+	        // 开始存款
+	        this.balance[account] += money;
 
-        return true;
+	        return true;	
+        }
     }
     
     public boolean withdraw(int account, long money) {
-        // 账号不存在
-        if (!this.exists(account)) return false;
+    	synchronized(this.balance) {
+    		// 账号不存在
+	        if (!this.exists(account)) return false;
 
-        // 钱不够
-        if (this.balance[account] < money) return false;
+	        // 钱不够
+	        if (this.balance[account] < money) return false;
 
-        // 开始取款
-        this.balance[account] -= money;
+	        // 开始取款
+	        this.balance[account] -= money;
 
-        return true;
+	        return true;	
+    	}
     }
 
     // 账号是否存在
     private boolean exists(int account) {
-        if (account < 1 || account > this.n) {
+        if (account < 1 || account > this.balance.length) {
             return false;
         }
         return true;
